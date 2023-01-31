ab39f76a3f3e231211f1c0a7e8ffed2629562a88
unknown <you@example.com>
2023-01-31 22:21:44 +0300

    task

---
+++ WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string About()
+        {
+            return "about us";
+        }
+
+        public string Contact()
+        {
+            return  "contact";
+        }
+
+        public string showimage()
+
+        { return "<a href=\"DownloadImage\"><img src=\"../img/oo.jpg\"> <a/>";  }
+
+        public FileResult DownloadImage()
+        {
+            var filePath = Server.MapPath("~/img/" + "oo.jpg");
+            return File(filePath, "image/jpeg", "oo.jpg");
+
+           
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/lubna.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class lubna : Controller
+    {
+        // GET: Default1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string name()
+        {
+            return "lubna";
+        }
+
+        public string city()
+        {
+            return "jordan";
+        }
+
+        public string age()
+        {
+            return "23 year";
+        }
+
+        public string email()
+        {
+            return "lubna.ajlouni@yahoo.com";
+        }
+
+    }
+}
