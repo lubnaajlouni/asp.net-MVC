0560288c61b1b8970c063763a11f064670478509
unknown <you@example.com>
2023-02-02 00:57:00 +0300

    task

---
+++ task2mvc/task2mvc/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using task2mvc.Models;
+
+namespace task2mvc.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+      
+
+        public ActionResult show()
+        {
+            List<Models.student> students = new List<Models.student>();
+
+            students.Add(new Models.student() { ID = 1, Name = "lubna", Major = "PlantProduction", Faculty = "Agricultural", });
+            students.Add(new Models.student() { ID = 2, Name = "nesreen", Major = "SoftwareEngineering", Faculty = "IT", });
+            students.Add(new Models.student() { ID = 2, Name = "Weesam", Major = "CivialEngineering", Faculty = "Engineering", });
+
+            return View(students);
+        }
+
+    }
+}
---
+++ task2mvc/task2mvc/Models/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace task2mvc.Models
+{
+    public class student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public string Faculty { get; set; }
+
+    }
+}
